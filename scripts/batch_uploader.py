ca5b273c91483465c5604e0b2ae1e46eda03149c
techXgood <technology.x.good@gmail.com>
2024-12-06 18:52:40 +0100

    chore: Add script to automatically upload big csv file with project urls

---
+++ scripts/batch_uploader.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+
+# Input and output file paths
+input_csv = "crawled_projects.csv"  # Input CSV file
+projects_file = "../data/projects.json"
+output_file = projects_file       # Output JSON file
+
+# Open and process the CSV file
+with open(projects_file, mode="r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Open and process the CSV file
+with open(input_csv, mode="r", encoding="utf-8") as file:
+    reader = csv.DictReader(file, delimiter=';')  # Automatically reads the header
+    for row in reader:
+        print(row)
+        new_obj = {
+            "repo": row["GitHub URL"],
+            "category": row["Category"],
+            "image": row["Preview Image"],
+        }
+        data.append(new_obj)  # Convert each row to a dictionary and append
+
+# Save the list of dictionaries to a file
+with open(output_file, mode="w", encoding="utf-8") as json_file:
+    json.dump(data, json_file, indent=4)
+
+# Remove input file
+
+print(f"Data saved to {output_file}.")
