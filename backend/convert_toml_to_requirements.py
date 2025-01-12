941371f5bfde0676992e6735998075b41a89a3e7
techXgood <technology.x.good@gmail.com>
2025-01-12 03:09:10 +0100

    ci: Add a script and the relative action to create the requirements.txt file on each push

---
+++ backend/convert_toml_to_requirements.py
@@ -0,0 +1,30 @@
+from typing import *
+import tomllib
+
+
+def convert_toml_to_requirements_txt(cwd: Optional[str] = '.'):
+    with open(f"{cwd}/pyproject.toml", "r") as file:
+        pyproject = tomllib.load(file)
+
+    # Extract dependencies
+    dependencies = pyproject.get("tool", {}).get("poetry", {}).get("dependencies", {})
+    dev_dependencies = pyproject.get("tool", {}).get("poetry", {}).get("dev-dependencies", {})
+
+    # Remove Python version constraint from dependencies
+    if "python" in dependencies:
+        del dependencies["python"]
+
+    # Write to requirements.txt
+    with open(f"{cwd}/requirements.txt", "w") as req_file:
+        for package, version in dependencies.items():
+            if version[0] == '^':
+                version = version[1:]
+            req_file.write(f"{package}=={version}\n")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) > 0:
+        cwd = sys.argv[-1]
+    convert_toml_to_requirements_txt(cwd)
