f7a608fd3e059c6cc89b6ff579e6f767f2f24ceb
techXgood <technology.x.good@gmail.com>
2025-02-19 23:48:33 +0100

    fix: Order of css imports; add another parameter to "include" function to choice where put the included code

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
-function include(file, selector) {
+function include(file, selector, where = "bottom") {
     fetch(file).then(response => response.text()).then(data => {
-            document.querySelector(selector).innerHTML = document.querySelector(selector).innerHTML.concat("\n", data);
+            let inner_html = document.querySelector(selector).innerHTML
+            if (where == "bottom") {
+                document.querySelector(selector).innerHTML = inner_html.concat("\n", data);
+            } else if (where == "top") {
+                document.querySelector(selector).innerHTML = data.concat("\n", inner_html);
+            } else {
+                throw "Unknown value for 'where' parameter";
+            }
         })
 };
